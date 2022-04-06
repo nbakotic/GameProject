a9e2f9cdc5f74010336d4aa9ed8701a0e0df7455
nbakotic <nbakotic@riteh.hr>
2022-04-06 17:45:37 +0200

    Added basic player controller and collision detection.

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,123 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [Header("Components")]
+    private Rigidbody2D _rb;
+
+    [Header("Layer Masks")]
+    [SerializeField] private LayerMask _groundLayer;
+
+    [Header("Movement Variables")]
+    [SerializeField] private float _movementAcceleration;
+    [SerializeField] private float _maxMoveSpeed;
+    [SerializeField] private float _groundLinearDrag;
+    private float _horizontalDirection;
+    private bool _changingDirection => (_rb.velocity.x > 0f && _horizontalDirection < 0f) || (_rb.velocity.x < 0f && _horizontalDirection > 0f);
+
+    [Header("Jump Variables")]
+    [SerializeField] private float _jumpForce = 12f;
+    [SerializeField] private float _airLienarDrag = 2.5f;
+    [SerializeField] private float _fallMultiplier = 8f;
+    [SerializeField] private float _lowJumpFallMultiplier = 5f;
+    private bool _canJump => Input.GetButtonDown("Jump") && _onGround;
+
+    [Header("Ground Collision Variables")]
+    [SerializeField] private float _groundRaycastLength;
+    [SerializeField] private Vector3 _groundRaycastOffset;
+    private bool _onGround;
+    
+    void Start()
+    {
+        _rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        _horizontalDirection = GetInput().x;
+        if (_canJump) Jump();
+    }
+
+    private void FixedUpdate()
+    {
+        CheckCollisions();
+        MoveCharacter();
+        if (_onGround)
+        {
+            ApplyGroundLinearDrag();
+        }
+        else
+        {
+            ApplyAirLinearDrag();
+            FallMultiplier();
+        }
+    }
+
+    private Vector2 GetInput()
+    {
+        return new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+    }
+
+
+    private void MoveCharacter()
+    {
+        _rb.AddForce(new Vector2(_horizontalDirection, 0f) * _movementAcceleration);
+
+        if (Mathf.Abs(_rb.velocity.x) > _maxMoveSpeed)
+            _rb.velocity = new Vector2(Mathf.Sign(_rb.velocity.x) * _maxMoveSpeed, _rb.velocity.y);
+    }
+
+    private void ApplyGroundLinearDrag()
+    {
+        if (Mathf.Abs(_horizontalDirection) < 0.4f || _changingDirection)
+        {
+            _rb.drag = _groundLinearDrag;
+        }
+        else
+        {
+            _rb.drag = 0f;
+        }
+    }
+
+    private void ApplyAirLinearDrag()
+    {
+        _rb.drag = _airLienarDrag;
+    }
+
+    private void Jump()
+    {
+        _rb.velocity = new Vector2(_rb.velocity.x, 0f);
+        _rb.AddForce(Vector2.up * _jumpForce, ForceMode2D.Impulse);
+    }
+
+    private void CheckCollisions()
+    {
+        _onGround = Physics2D.Raycast(transform.position + _groundRaycastOffset, Vector2.down, _groundRaycastLength, _groundLayer) ||
+                    Physics2D.Raycast(transform.position - _groundRaycastOffset, Vector2.down, _groundRaycastLength, _groundLayer);
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.green;
+        Gizmos.DrawLine(transform.position, transform.position + Vector3.down * _groundRaycastLength);
+    }
+
+    private void FallMultiplier()
+    {
+        if (_rb.velocity.y < 0)
+        {
+            _rb.gravityScale = _fallMultiplier;
+        }
+        else if (_rb.velocity.y > 0 && !Input.GetButton("Jump"))
+        {
+            _rb.gravityScale = _lowJumpFallMultiplier;
+        }
+        else
+        {
+            _rb.gravityScale = 1f;
+        }
+    }
+}
