ae6c872fbf8c5456ee029e87885298e3d1c1b66e
Matej Anić <matej.anic10@gmail.com>
2022-04-27 00:49:12 +0200

    Added enemy moving between two walls

---
+++ Assets/Scripts/GuardEnemyController.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GuardEnemyController : MonoBehaviour
+{
+    [Header("Components")]
+    private Rigidbody2D rb;
+    
+    [Header("Movement variables")]
+    public float enemyVelocity = 2.0f;
+
+
+    
+
+
+    void Start()
+    {
+        rb = this.GetComponent<Rigidbody2D>();
+    }
+
+    
+    void FixedUpdate()
+    {
+        rb.velocity = new Vector2(enemyVelocity, 0f);
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        enemyVelocity *= -1; 
+    }
+}
