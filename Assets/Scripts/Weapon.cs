d2478a3eb4ebd38652f7e363ef5450f081c6ec04
aZina0 <ankz5959@gmail.com>
2022-05-12 16:46:41 +0200

    Normalized the aim vector. Fixes #17

--- Assets/Scripts/Weapon.cs
+++ Assets/Scripts/Weapon.cs
@@ -17,15 +17,24 @@ public class Weapon : MonoBehaviour
 
     void Update()
     {
+        // Get the position of the mouse in the world coordinates
         Vector3 screenMousePos = Input.mousePosition;
         screenMousePos.z = 10f;
         Vector3 worldMousePos = Camera.main.ScreenToWorldPoint(screenMousePos);
-        transform.localRotation = Quaternion.FromToRotation(Vector3.right, worldMousePos - transform.parent.transform.position);
 
+        Vector3 aimVector = Vector3.Normalize(worldMousePos - transform.parent.transform.position);
+
+        // Rotate the weapon object towards the aim position
+        transform.localRotation = Quaternion.FromToRotation(Vector3.right, aimVector);
+
+        // If left mouse clicked
         if (Input.GetMouseButtonDown(0))
         {
+            // Create new projectile object and give it force
             GameObject projectileInst = Instantiate(projectile, projectileOrigin.transform.position, projectileOrigin.transform.rotation);
-            projectileInst.GetComponent<Rigidbody2D>().AddForce((worldMousePos - transform.parent.transform.position) * projectileVelocity);
+            projectileInst.GetComponent<Rigidbody2D>().AddForce(aimVector * projectileVelocity);
+
+            // If flag "despawnProjectileOnImpact" is enabled add a script to the projectile that despawns it on impact
             if (despawnProjectileOnImpact)
             {
                 projectileInst.AddComponent<Projectile>();
