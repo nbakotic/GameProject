9b13a1f320d2f3a53c630f19ae353183e325098c
AKStifter <95703322+AKStifter@users.noreply.github.com>
2022-05-18 19:03:02 +0200

    Moving platform
    Simple moving platform that moves between waypoints.

---
+++ Assets/Scripts/PlatformMovement.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformMovement : MonoBehaviour
+{
+    [SerializeField] private GameObject[] waypoints;
+    private int currentWaypointIndex = 0;
+
+    [SerializeField] private float speed = 2f;
+
+    private void Update()
+    {
+        //Controlls which waypoint is being moved towards
+        if (Vector2.Distance(waypoints[currentWaypointIndex].transform.position, transform.position) < .1f)
+        {
+            currentWaypointIndex = ++currentWaypointIndex % waypoints.Length;
+        }
+        //Moves the platform independent of framerate
+        transform.position = Vector2.MoveTowards(transform.position, waypoints[currentWaypointIndex].transform.position, Time.deltaTime * speed);
+    }
+}
